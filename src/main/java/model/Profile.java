14476202ebf6a1cf374cad5a90c98a5169a89fc0
Kaizzz-00 <15359435358zck@gmail.com>
2022-10-02 23:54:45 -0400

    final_version
    the final Verison

---
+++ src/main/java/model/Profile.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.Date;
+/**
+ *
+ * @author kai
+ */
+public class Profile {
+    private String name;
+    private String ID;
+    private Integer age;
+    private String gender;
+    private Date startdate;
+    private String level;
+    private String teamInfo;
+    private String postitle;
+    private String phone;
+    private String email;
+    private String photo;
+
+    public String getPhoto() {
+        return photo;// use the address of the image as attribute 'photo'
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(Date startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPostitle() {
+        return postitle;
+    }
+
+    public void setPostitle(String postitle) {
+        this.postitle = postitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+         
+    
+}
